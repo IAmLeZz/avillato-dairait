2d999886168597705ac225941bbc230a09af1a22
Alvaro Villalo <alvaro_villalo@hotmail.com>
2024-02-04 21:30:54 -0300

    2 horas + Se cambiaron numeros enteros por floats + Se empezó a utilizar CORS para la compatibilidad con los navegadores + Se reemplazaron los placeholders de Calculadora y Componente con código funcional + Se añadió un store para sincronizar los cambios del historial con cada request - Se arreglaran bugs y validación de datos

--- backend-golang/main.go
+++ backend-golang/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"math"
 	"net/http"
 	"time"
 
@@ -9,22 +11,22 @@ import (
 )
 
 type OperationRequest struct {
-	Operator string `json:"operator"`
-	Num1     int    `json:"num1"`
-	Num2     int    `json:"num2"`
+	Operator string  `json:"operator"`
+	Num1     float64 `json:"num1"`
+	Num2     float64 `json:"num2"`
 }
 
 type OperationResponse struct {
-	Result    int    `json:"result"`
-	Error     string `json:"error"`
-	Time      string `json:"time"`
-	Operation string `json:"operation"`
+	Result    float64 `json:"result"`
+	Error     string  `json:"error"`
+	Time      string  `json:"time"`
+	Operation string  `json:"operation"`
 }
 
 var history = make([]OperationResponse, 0)
 
-func calculate(operator string, num1 int, num2 int) (int, error) {
-	var result int
+func calculate(operator string, num1 float64, num2 float64) (float64, error) {
+	var result float64
 	var err error
 
 	switch operator {
@@ -37,7 +39,7 @@ func calculate(operator string, num1 int, num2 int) (int, error) {
 	case "/":
 		result = num1 / num2
 	case "%":
-		result = num1 % num2
+		result = math.Mod(num1, num2)
 	default:
 		err = errors.New("syntax error")
 	}
@@ -55,6 +57,7 @@ func postOperation(c *gin.Context) {
 	result, err := calculate(request.Operator, request.Num1, request.Num2)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err.Error())
 		return
 	}
 
@@ -79,13 +82,18 @@ func CORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
-		c.Next()
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 	}
 }
 
 func main() {
 	router := gin.Default()
+	router.Use(CORS())
 	router.POST("/operation", postOperation)
 	router.GET("/history", getHistory)
 	router.Run(":8080")
--- frontend-svelte/src/components/Calculadora.svelte
+++ frontend-svelte/src/components/Calculadora.svelte
@@ -1,42 +1,134 @@
 <script>
-   
-</script>
+    import { onMount } from 'svelte';
+    import { history } from './store.js'
+    let display = '';
+    let result = '';
+    let num1 = '';
+    let num2 = '';
+    let operator = '';
   
-<style>
+    const calculate = async () => {
+  if (num1 && num2 && operator) {
+    const response = await fetch('http://localhost:8080/operation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        operator: operator,
+        num1: Number(num1),
+        num2: Number(num2)
+      })
+    });
+    const res = await response.json();
+    result =  Math.round(res.result*100)/100;
+    num1 = '';
+    num2 = '';
+    operator = '';
+    console.log(result)
+    const newEntry = {
+        result: res.result,
+        time: res.time,
+        operation: res.operation,
+    }
+    history.update(oldHistory => [...oldHistory, newEntry]);
+  }
+}
+        const handleNumberClick = (number) => {
+        if (!operator) {
+            num1 = num1 + number;
+        } else {
+            num2 = num2 + number;
+        }
+        display = num1 + operator + num2;
+    }
+  
 
-</style>
+  const handleOperatorClick = (op) => {
+  if (result) {
+    num1 = result.toString();
+    result = '';
+  }
+  if (op === '-' && num1 === '') {
+    num1 = '-';
+    display = num1;
+  } else {
+    operator = op;
+    display = num1 + operator;
+  }
+}
+const clear = () => {
+    display = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+    result = '';
+}
   
+    const clearEntry = () =>{
+      if (!operator) {
+        num1 = '';
+      } else {
+        num2 = '';
+      }
+      display = '';
+      result = '';
+    }
+  
+    const changeSign = () => {
+      if (!operator) {
+        num1 = (-Number(num1)).toString();
+        display = num1;
+      } else {
+        num2 = (-Number(num2)).toString();
+        display = num2;
+      }
+    }
+
+    const handleDecimalClick = () => {
+  if (!operator) {
+    if (!num1.includes('.')) {
+      num1 += '.';
+    }
+  } else {
+    if (!num2.includes('.')) {
+      num2 += '.';
+    }
+  }
+  display = num1 + operator + num2;
+}
+  </script>
+
   <div class="calculator bg-white border border-gray-300 p-6 rounded-lg min-w-[350px]">
     <div class="display bg-black text-white p-2 rounded mb-4">
-      <p class="text-right min-h-6"></p>
-      <p class="text-right text-xl min-h-2"></p>
+      <p class="text-right min-h-6 text-white">{display} {(result) && (" = " + result)}</p>
     </div>
   
     <div class="grid grid-cols-4 gap-3">
-      <button class="bg-orange-500 text-white p-4 rounded">CE</button>
-      <button class="bg-orange-500 text-white p-4 rounded">C</button>
-      <button class="bg-red-500 text-white p-4 rounded">+</button>
-      <button class="bg-red-500 text-white p-4 rounded">%</button>
+      <button class="bg-orange-500 text-white p-4 rounded" on:click={clearEntry}>CE</button>
+      <button class="bg-orange-500 text-white p-4 rounded" on:click={clear}>C</button>
+      <button class="bg-red-500 text-white p-4 rounded" on:click={changeSign}>±</button>
+      <button class="bg-red-500 text-white p-4 rounded" on:click={() => handleOperatorClick('%')}>%</button>
   
-      <button class="bg-blue-500 text-white p-4 rounded">7</button>
-      <button class="bg-blue-500 text-white p=4 rounded">8</button> 
-      <button class="bg-blue-500 text-white p=4 rounded">9</button> 
-      <button class="bg-red-800 text-white p=4 rounded">/</button> 
+      <button class="bg-blue-500 text-white p-4 rounded" on:click={() => handleNumberClick('7')}>7</button>
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('8')}>8</button> 
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('9')}>9</button> 
+      <button class="bg-red-800 text-white p=4 rounded" on:click={() => handleOperatorClick('/')}>/</button> 
 
-      <button class="bg-blue-500 text-white p-4 rounded">4</button>
-      <button class="bg-blue-500 text-white p=4 rounded">5</button> 
-      <button class="bg-blue-500 text-white p=4 rounded">6</button> 
-      <button class="bg-red-800 text-white p=4 rounded">x</button> 
+      <button class="bg-blue-500 text-white p-4 rounded" on:click={() => handleNumberClick('4')}>4</button>
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('5')}>5</button> 
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('6')}>6</button> 
+      <button class="bg-red-800 text-white p=4 rounded" on:click={() => handleOperatorClick('*')}>x</button> 
 
-      <button class="bg-blue-500 text-white p-4 rounded">3</button>
-      <button class="bg-blue-500 text-white p=4 rounded">2</button> 
-      <button class="bg-blue-500 text-white p=4 rounded">1</button> 
-      <button class="bg-red-800 text-white p=4 rounded">-</button> 
+      <button class="bg-blue-500 text-white p-4 rounded" on:click={() => handleNumberClick('3')}>3</button>
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('2')}>2</button> 
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={() => handleNumberClick('1')}>1</button> 
+      <button class="bg-red-800 text-white p=4 rounded" on:click={() => handleOperatorClick('-')}>-</button> 
        
-      <button class="bg-blue-500 text-white p-4 rounded">0</button>
-      <button class="bg-blue-500 text-white p=4 rounded">.</button> 
-      <button class="bg-green-700 text-white p=4 rounded">=</button> 
-      <button class="bg-red-800 text-white p=4 rounded">+</button> 
+      <button class="bg-blue-500 text-white p-4 rounded" on:click={() => handleNumberClick('0')}>0</button>
+      <button class="bg-blue-500 text-white p=4 rounded" on:click={()=> handleDecimalClick()}>.</button> 
+      <button class="bg-green-700 text-white p=4 rounded" on:click={calculate}>=</button>
+      <button class="bg-red-800 text-white p=4 rounded" on:click={() => handleOperatorClick('+')}>+</button>
        
     </div>
   </div>
